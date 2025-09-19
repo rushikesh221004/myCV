8550c1f5abd31df8dbf8f255b4bfea1a7950b24b
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-09-20 02:23:36 +0530

    about thumb fix

--- src/components/AboutThumb.jsx
+++ src/components/AboutThumb.jsx
@@ -1,8 +1,8 @@
 import aboutThumb from "../assets/images/aboutThumbImg.png"
 export const AboutThumb = () => {
   return (
-    <div className="w-full xl:w-[50%] flex justify-center">
-        <div className="h-[500px] xl:w-[450px] w-[300px] sm:w-[400px] sm:h-[600px] md:h-[700px] md:w-[500px] relative aboutThumb overflow-hidden bg-white rounded-full">
+    <div className="w-full xl:justify-start xl:w-[50%] flex justify-center">
+        <div className="h-[500px] xl:w-[450px] 2xl:w-[500px] 2xl:h-[800px] w-[300px] sm:w-[400px] sm:h-[600px] md:h-[700px] md:w-[500px] relative aboutThumb overflow-hidden bg-white rounded-full">
             <img src={aboutThumb} alt="aboutThumb" className="h-full top-[50px] left-1/2 -translate-x-1/2 absolute rounded-full object-top object-cover"/>
         </div>
     </div>
