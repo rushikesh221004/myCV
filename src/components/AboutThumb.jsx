56f32ace8d30601df688711ca97dba01d45c26f8
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-09-20 01:39:27 +0530

    about thumb created

---
+++ src/components/AboutThumb.jsx
@@ -0,0 +1,10 @@
+import aboutThumb from "../assets/images/aboutThumbImg.png"
+export const AboutThumb = () => {
+  return (
+    <div className="w-full xl:w-[50%] flex justify-center">
+        <div className="h-[500px] xl:w-[450px] w-[300px] sm:w-[400px] sm:h-[600px] md:h-[700px] md:w-[500px] relative aboutThumb overflow-hidden bg-white rounded-full">
+            <img src={aboutThumb} alt="aboutThumb" className="h-full top-[50px] left-1/2 -translate-x-1/2 absolute rounded-full object-top object-cover"/>
+        </div>
+    </div>
+  )
+}
